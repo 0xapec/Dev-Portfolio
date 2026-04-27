b1b5d665c2ec0b1f00eed8aaddf54e84ffe2011c
0xapec <anahothem@outlook.com>
2026-04-26 22:20:42 -0400

    remove some comments

--- src/portfolio.js
+++ src/portfolio.js
@@ -34,14 +34,8 @@ const greeting = {
 
 const socialMediaLinks = {
   github: "https://github.com/0xapec",
-  // linkedin: "https://www.linkedin.com/in/saadpasta/",
   gmail: "anahothem@outlook.com",
-  // gitlab: "https://gitlab.com/saadpasta",
-  // facebook: "https://www.facebook.com/saad.pasta7",
   medium: "https://medium.com/@0xapec",
-  // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
-  // Instagram, Twitter and Kaggle are also supported in the links!
-  // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
 };
 
